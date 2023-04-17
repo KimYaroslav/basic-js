71b881fefcd77fab02d2ca000d6b2c1c22015765
KimYaroslav <Korean_mafia_01@mail.ru>
2023-04-18 01:26:33 +0600

    fix: fix transform-array errors

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Create transformed array based on the control sequences that original
  * array contains
@@ -14,76 +12,108 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
+  console.log(arr);
   if (!Array.isArray(arr)) {
+    console.log(0);
     throw new Error('\'arr\' parameter must be an instance of the Array!');
   }
 
-  if (!(arr.includes('--discard-next') ||
-    arr.includes('--discard-prev') ||
-    arr.includes('--double-next') ||
-    arr.includes('--double-prev'))) {
+  if (!arr.includes('--discard-next') &&
+    !arr.includes('--discard-prev') &&
+    !arr.includes('--double-next') &&
+    !arr.includes('--double-prev')) {
+    console.log(1);
     return arr;
   }
 
   let newArr = [];
+  
+  for (let i = 0; i < arr.length; i++) {
 
-  if (arr.length !== 0) {
+    if (typeof (arr[i]) === 'number') {
+      newArr.push(arr[i]);
+      console.log(2);
+      continue;
+    }
 
-    for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next' && arr[i + 2] === '--double-prev') {
+      i = i + 2;
+      console.log(3);
+      continue;
+    }
 
-      if (typeof (arr[i]) === 'number') {
-        newArr.push(arr[i]);
-        continue;
-      }
-
-      if (arr[i] === '--discard-next' && arr[i + 2] === '--double-prev') {
-        i = i + 2;
-        continue;
-      }
-
-      if (arr[i] === '--double-next' && arr[i + 2] === '--double-prev') {
-        for (let j = 0; j < 3; j++) {
-          newArr.push(arr[i + 1]);
-        };
-
-        i = i + 2;
-        continue;
-      }
-
-      if (arr[i] === '--discard-next' && arr[i + 2] === '--discard-prev') {
-        i = i + 2;
-        continue;
-      }
-
-      if (arr[i] === '--double-next' && arr[i + 2] === '--discard-prev') {
+    if (arr[i] === '--double-next' && arr[i + 2] === '--double-prev') {
+      for (let j = 0; j < 3; j++) {
         newArr.push(arr[i + 1]);
-        i = i + 2;
-        continue;
-      }
+        console.log(3);
+      };
 
-      else if (i !== 0) {
-        if (arr[i] === '--discard-prev') {
-          newArr.slice(i - 1, 1);
-        }
-        else if (arr[i] === '--double-prev') {
-          newArr.push(arr[i - 1]);
-        }
-        continue;
-      }
+      i = i + 2;
+      console.log(4);
+      continue;
+    }
 
-      else if (i !== (arr.lenght - 1)) {
-        if (arr[i] === '--double-next') {
-          newArr.push(arr[i + 1]);
-        }
-        else if (arr[i] === '--discard-next') {
-          i = i + 1;          
-        }
+    if (arr[i] === '--discard-next' && arr[i + 2] === '--discard-prev') {
+      i = i + 2;
+      console.log(5);
+      continue;
+    }
+
+    if (arr[i] === '--double-next' && arr[i + 2] === '--discard-prev') {
+      newArr.push(arr[i + 1]);
+      i = i + 2;
+      console.log(6);
+      continue;
+    }
+
+    else if (arr[i] === '--discard-prev') {
+      if (i === 0) {
+        console.log(7);
+        continue;
+      } else {
+        newArr.slice(i - 1, 1);
+        console.log(8);
         continue;
       }
     }
-    return newArr;
+
+    else if (arr[i] === '--double-prev') {
+      if (i === 0) {
+        console.log(9);
+        continue;
+      } else {
+        newArr.push(arr[i - 1]);
+        console.log(10);
+        continue;
+      }
+    }
+
+    else if (arr[i] === '--double-next') {
+      if (i === (arr.length - 1)) {
+        console.log(11);
+        continue;
+      } else {
+        newArr.push(arr[i + 1]);
+        console.log(12);
+        continue;
+      }
+    }
+
+    else if (arr[i] === '--discard-next') {
+      if (arr[i] === (arr.length - 1)) {
+        console.log(13);
+        continue;
+      } else {
+        i = i + 1;
+        console.log(14);
+        continue;
+      }
+    }
   }
+  return newArr;
 }
+
+
 
 module.exports = {
   transform
